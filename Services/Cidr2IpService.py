eaa35c63a92f8b053fc163c44d363169d8312550
BoxHezi <547181293@qq.com>
2023-09-17 16:10:35 +1000

    add todo

--- Services/Cidr2IpService.py
+++ Services/Cidr2IpService.py
@@ -3,4 +3,9 @@ from Module.Cidr2Ip import CIDR2IP
 
 
 def dao_2_obj(dao: Cidr2IpData) -> CIDR2IP:
+    # TODO: convert Cidr2IpData to CIDR2IP object
+    pass
+
+def cidr_2_ip_to_database(db, cidr2ip_obj: CIDR2IP):
+    # TODO: insert/update cidr2ip_obj to database
     pass
